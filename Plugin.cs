de43f3002829c507bc9999666f1c6092eec3c8d5
ReallyBadDeveloper <reallybaddeveloper@gmail.com>
2025-01-03 13:02:46 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.CompilerServices;
+using BepInEx;
+using TMPro;
+using UnityEngine;
+
+namespace SimpleUILib
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Canvas canvas;
+        internal static int nextId = 0;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void Update()
+        {
+            // name of canvas is "Canvas (1)" because zapray makes messy gameobjects smh
+            canvas = GameObject.Find("Canvas (1)").GetComponent<Canvas>();
+            if (nextId == 0) {
+                // Testing time!!!!!!! :realization:
+                new Text("Hello World", new Vector2(0, 0), new Vector2(7, 0.5f), Color.black);
+            }
+        }
+        private void OnSceneLoad()
+        {
+
+        }
+    }
+    public class Text
+    {
+        public int id;
+        public string text;
+        public Vector2 pos;
+        public Vector2 size;
+        public GameObject gameObject;
+        public RectTransform rectTransform;
+        public TextMeshProUGUI tmpComponent;
+        public Color color;
+        public Text(string text, Vector2 position, Vector2 size, Color color)
+        {
+            this.text = text;
+            this.pos = position;
+            this.size = size;
+            this.id = Plugin.nextId;
+            this.color = color;
+            Plugin.nextId++;
+            gameObject = new GameObject("TEXT_" + this.id.ToString());
+            gameObject.transform.SetParent(Plugin.canvas.transform);
+            gameObject.transform.position = pos;
+            gameObject.AddComponent<TextMeshProUGUI>();
+            tmpComponent = gameObject.GetComponent<TextMeshProUGUI>();
+            tmpComponent.font = LocalizedText.localizationTable.GetFont(Settings.Get().Language, false);
+            tmpComponent.color = color;
+            tmpComponent.fontSize = 0.27f;
+            tmpComponent.alignment = TextAlignmentOptions.TopLeft;
+            gameObject.GetComponent<TextMeshProUGUI>().text = text;
+            gameObject.AddComponent<RectTransform>();
+            rectTransform = gameObject.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(1, 0);
+            rectTransform.anchorMax = new Vector2(1, 0);
+            rectTransform.pivot = new Vector2(1, 0);
+            rectTransform.anchoredPosition = pos;
+            rectTransform.sizeDelta = this.size;
+        }
+
+    }
+}
---
+++ SpriteLoader.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+// https://forum.unity.com/threads/generating-sprites-dynamically-from-png-or-jpeg-files-in-c.343735/
+
+namespace SimpleUILib
+{
+    public class IMG2Sprite : MonoBehaviour
+    {
+        //https://forum.unity.com/threads/generating-sprites-dynamically-from-png-or-jpeg-files-in-c.343735/
+        // This script loads a PNG or JPEG image from disk and returns it as a Sprite
+        // Drop it on any GameObject/Camera in your scene (singleton implementation)
+        //
+        // Usage from any other script:
+        // MySprite = IMG2Sprite.instance.LoadNewSprite(FilePath, [PixelsPerUnit (optional)])
+
+        private static IMG2Sprite _instance;
+
+        public static IMG2Sprite instance
+        {
+            get
+            {
+                //If _instance hasn't been set yet, we grab it from the scene!
+                //This will only happen the first time this reference is used.
+
+                if (_instance == null)
+                    _instance = GameObject.FindObjectOfType<IMG2Sprite>();
+                return _instance;
+            }
+        }
+
+        public static Sprite LoadNewSprite(byte[] FileData, float PixelsPerUnit = 100.0f)
+        {
+
+            // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+
+            Sprite NewSprite = new Sprite();
+            Texture2D SpriteTexture = LoadTexture(FileData);
+            var rect = new Rect(0, 0, SpriteTexture.width, SpriteTexture.height);
+            float pivotX = ((float)SpriteTexture.width / 2) / (float)SpriteTexture.width; // Convert pixel coordinate to Unity units
+            float pivotY = ((float)SpriteTexture.height / 2) / (float)SpriteTexture.height; // Convert pixel coordinate to Unity units
+
+            // Create the sprite with the correct pivot point
+            Vector2 vector = new Vector2(pivotX, pivotY);
+            float PixelsPerUnit2 = Mathf.Max((float)SpriteTexture.width, (float)SpriteTexture.height);
+
+            Debug.Log($"vector is {vector}");
+            NewSprite = Sprite.Create(SpriteTexture, rect, vector, PixelsPerUnit);
+
+            return NewSprite;
+        }
+
+        public static Texture2D LoadTexture(byte[] FileData)
+        {
+
+            // Load a PNG or JPG file from disk to a Texture2D
+            // Returns null if load fails
+
+            Texture2D Tex2D;
+            Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
+            if (Tex2D.LoadImage(FileData))           // Load the imagedata into the texture (size is set automatically)
+                return Tex2D;                 // If data = readable -> return texture
+            return null;                     // Return null if load failed
+        }
+    }
+}
